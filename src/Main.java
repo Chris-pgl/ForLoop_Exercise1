ff836eafef7170429453c076538137b6afe5cddd
Chris <cr1s@hotmail.it>
2023-01-12 13:22:10 +0100

    Add for loop exercise 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for ( int numero = 5; numero <= 25; numero++){
+            System.out.println(numero);
+        }
+    }
+}
